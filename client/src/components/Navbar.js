49c2bd7c173436ec091d531160e9339a4cc18306
NoelleDeAngelo <NoelleDeAngelo@gmail.com>
2022-10-20 17:11:45 -0700

    add css for nav bar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -3,7 +3,7 @@ import {Link} from "react-router-dom"
 
 export default function Navbar(){
   return <nav className= "nav">
-     <Link to= '/'>ProjectAC</Link>
+     <Link className= "site-name" to= '/'>ProjectAC</Link>
     <ul>
       <Link to= '/home'>Home</Link>
       <Link to= '/myclasses'>MyClasses</Link>
